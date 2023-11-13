6841cf4bc32fa0e80d6a9651a727f3a14195e60e
hwi9yeom <hwi9yeom@gmail.com>
2023-11-13 23:59:38 +0900

    feat: Fix test code, add db, jpa

--- src/test/java/com/example/demo/web/HelloControllerTest.java
+++ src/test/java/com/example/demo/web/HelloControllerTest.java
@@ -1,6 +1,6 @@
 package com.example.demo.web;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest
 public class HelloControllerTest {
 
     @Autowired
