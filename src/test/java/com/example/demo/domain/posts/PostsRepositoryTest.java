e7ae422fa11ce6b5876dd9dd3ded6f9a2ffb859d
hwi9yeom <hwi9yeom@gmail.com>
2023-11-14 23:30:50 +0900

    feat: Add domain.posts & test

---
+++ src/test/java/com/example/demo/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,46 @@
+package com.example.demo.domain.posts;
+
+import org.assertj.core.util.Lists;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("test@test.com")
+                .build());
+
+        //when
+        Lists<Posts> postsLists = postsRepository.findAll();
+
+        //then
+        Posts posts =  postsLists.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+
+    }
+}
